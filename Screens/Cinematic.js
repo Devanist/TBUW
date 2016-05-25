1d8330582570a5dfe07d73960292f44ec7f71216
Devanist <devanist@outlook.com>
2016-05-25 15:02:28 +0200

    fadeout screen after cinematic is finished

--- Screens/Cinematic.js
+++ Screens/Cinematic.js
@@ -38,63 +38,73 @@ function(Screen){
     };
     
     _p.update = function(){
-        var diff = 0;
-        if(this._loaded){
-            if(this._currentAnimation < this._cinematicConfig.length){
-                
-                this._animatedObject = this._stage.getElement(this._cinematicConfig[this._currentAnimation].id);
-                
-                if(this._animationComplete === false){
-                    if(this._cinematicConfig[this._currentAnimation].moveTo.time === 0){
-                        this._animatedObject.setPosition({x: this._cinematicConfig[this._currentAnimation].moveTo.x, y: this._cinematicConfig[this._currentAnimation].moveTo.y});
-                        this._animationComplete = true;
-                    }
-                    else{
-                        if(this._stepCounter === 0){
-                            this._step = {
-                                x: -(this._animatedObject.getPosition().x - this._cinematicConfig[this._currentAnimation].moveTo.x) / this._cinematicConfig[this._currentAnimation].moveTo.time,
-                                y: -(this._animatedObject.getPosition().y - this._cinematicConfig[this._currentAnimation].moveTo.y) / (this._cinematicConfig[this._currentAnimation].moveTo.time / 16.666)
-                            };
-                        }
-                        this._animatedObject.move(this._step);
-                        console.log('x:' + this._animatedObject._sprite.position.x + ' y:' + this._animatedObject._sprite.position.y);
-                        this._stepCounter++;
-                        if(this._stepCounter >= this._cinematicConfig[this._currentAnimation].moveTo.time / 16.666){
+        
+        if(!this._finished){
+        
+            var diff = 0;
+            if(this._loaded){
+                if(this._currentAnimation < this._cinematicConfig.length){
+                    
+                    this._animatedObject = this._stage.getElement(this._cinematicConfig[this._currentAnimation].id);
+                    
+                    if(this._animationComplete === false){
+                        if(this._cinematicConfig[this._currentAnimation].moveTo.time === 0){
                             this._animatedObject.setPosition({x: this._cinematicConfig[this._currentAnimation].moveTo.x, y: this._cinematicConfig[this._currentAnimation].moveTo.y});
                             this._animationComplete = true;
-                            this._beginTime = 0;
-                            this._stepCounter = 0;
+                        }
+                        else{
+                            if(this._stepCounter === 0){
+                                this._step = {
+                                    x: -(this._animatedObject.getPosition().x - this._cinematicConfig[this._currentAnimation].moveTo.x) / this._cinematicConfig[this._currentAnimation].moveTo.time,
+                                    y: -(this._animatedObject.getPosition().y - this._cinematicConfig[this._currentAnimation].moveTo.y) / (this._cinematicConfig[this._currentAnimation].moveTo.time / 16.666)
+                                };
+                            }
+                            this._animatedObject.move(this._step);
+                            console.log('x:' + this._animatedObject._sprite.position.x + ' y:' + this._animatedObject._sprite.position.y);
+                            this._stepCounter++;
+                            if(this._stepCounter >= this._cinematicConfig[this._currentAnimation].moveTo.time / 16.666){
+                                this._animatedObject.setPosition({x: this._cinematicConfig[this._currentAnimation].moveTo.x, y: this._cinematicConfig[this._currentAnimation].moveTo.y});
+                                this._animationComplete = true;
+                                this._beginTime = 0;
+                                this._stepCounter = 0;
+                            }
                         }
                     }
-                }
-                
-                if(this._animationComplete === true){
-                    if(this._cinematicConfig[this._currentAnimation].moveTo.wait === 0){
-                        this._currentAnimation++;
-                        this._animationComplete = false;
-                    }
-                    else{
-                        if(this._beginTime === 0){
-                            this._beginTime = Date.now();
-                        }
-                        this._currentTime = Date.now();
-                        diff = this._currentTime - this._beginTime;
-                        if(diff >= this._cinematicConfig[this._currentAnimation].moveTo.wait){
+                    
+                    if(this._animationComplete === true){
+                        if(this._cinematicConfig[this._currentAnimation].moveTo.wait === 0){
                             this._currentAnimation++;
                             this._animationComplete = false;
                         }
+                        else{
+                            if(this._beginTime === 0){
+                                this._beginTime = Date.now();
+                            }
+                            this._currentTime = Date.now();
+                            diff = this._currentTime - this._beginTime;
+                            if(diff >= this._cinematicConfig[this._currentAnimation].moveTo.wait){
+                                this._currentAnimation++;
+                                this._animationComplete = false;
+                                this._step = 0;
+                            }
+                        }
                     }
+                    
                 }
-                
-            }
-            else{
-                this._finished = true;
+                else{
+                    this._finished = true;
+                }
             }
         }
-        
-        if(this._finished){
-            this._onUpdateAction = "CHANGE";
-            this._nextScreen = "game";
+        else{
+            if(this._stage._stage.alpha > -1){
+                this._stage._stage.alpha -= 0.01;
+                console.log(this._stage._stage.alpha);
+            }
+            else{
+                this._onUpdateAction = "CHANGE";
+                this._nextScreen = "game";
+            }
         }
         return {action: this._onUpdateAction, changeTo: this._nextScreen, playSound: this._sounds};
     };

477a080de796da7863bc67157e10a669d43c2ce9
Devanist <devan93@gmail.com>
2016-08-07 13:18:46 +0200

    changed c2_l1 to test level, fixed c1_l1 typo, changed initial screen for testing

--- Assets/Levels/Levels.js
+++ Assets/Levels/Levels.js
@@ -1,10 +1,12 @@
 define([
-    "json!Assets/Levels/c1_l1.json"
+    "json!Assets/Levels/c1_l1.json",
+    "json!Assets/Levels/test.json"
 ], 
-function(c1_l1){
+function(c1_l1, test){
 
     var Levels = {
-        C1_L1: c1_l1
+        C1_L1: c1_l1,
+        test: test
     };
 
     return Levels;

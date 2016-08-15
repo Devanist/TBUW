5bf4a13c758059da4c896657b1458c4a5593a116
Devanist <devanist@outlook.com>
2016-08-16 01:24:19 +0200

    you can trigger pause buttons with keyboard

--- Screens/Game.js
+++ Screens/Game.js
@@ -20,6 +20,20 @@ define([
         this._music = null;
         this._back = params.back;
         this._retry = params;
+
+        this._buttonPressedDown = false;
+
+        this._displacementmap = PIXI.Sprite.fromImage("Assets/Gfx/displacement_map.png");
+        this._displacementmap.r = 1;
+        this._displacementmap.g = 1;
+        this._displacement = new PIXI.filters.DisplacementFilter(this._displacementmap);
+        this._displacement.scale.x = 1.5;
+        this._displacement.scale.y = 2;
+        this._displacement.offset = {
+            x: 0,
+            y: 0
+        };
+
         
         this._small = 1;
         if(window.innerWidth <= 640){
@@ -267,7 +281,7 @@ define([
                 }
 
             }
-    }
+    };
         
     /**
      * Metoda przygotowująca dane i wysyłająca je do workera.
@@ -352,13 +366,91 @@ define([
         
         }
         else{
-            l = this._guiStage._elements.length;
-            for(let i = 0; i < l; i+=1){
+
+            if(keysState.ARROW_DOWN || keysState.S){
+                if(this._buttonPressedDown === false){
+                    this._buttonPressedDown = true;
+                    while(i != 2){
+                        if(j == this._guiStage._elements.length){
+                            j = 0;
+                        }
+                        temp = this._guiStage._elements[j];
+                        if(temp.isEnabled() && temp.isActive()){
+                            temp._data.active = false;
+                            temp._text.filters = null;
+                            i = 1;
+                            j+=1;
+                            continue;
+                        }
+                        if(i == 1 && temp.isEnabled()){
+                            temp._data.active = true;
+                            i = 2;
+                        }
+                        else{
+                            j+=1;
+                        }
+                    }
+                }
+            }
+            
+            if(keysState.ARROW_UP || keysState.W){
+                if(this._buttonPressedDown === false){
+                    this._buttonPressedDown = true;
+                    while(i != 2){
+                        if(j == -1){
+                            j = this._guiStage._elements.length - 1;
+                        }
+                        temp = this._guiStage._elements[j];
+                        if(temp.isEnabled() && temp.isActive()){
+                            temp._data.active = false;
+                            temp._text.filters = null;
+                            i = 1;
+                            j-=1;
+                            continue;
+                        }
+                        if(i == 1 && temp.isEnabled()){
+                            temp._data.active = true;
+                            i = 2;
+                        }
+                        else{
+                            j-=1;
+                        }
+                    }
+                }
+            }
+            
+            if(keysState.ENTER){
+                for(let i = 0; i < this._guiStage._elements.length; i+=1){
+                    temp = this._guiStage._elements[i];
+                    if(temp && temp.isActive()){
+                        temp.triggerCallback();
+                    }
+                }
+            }
+            
+            if(!keysState.ARROW_DOWN && !keysState.S && !keysState.ARROW_UP && !keysState.W){
+                this._buttonPressedDown = false;
+            }
+
+            for(let i = 0; i < this._guiStage._elements.length; i+=1){
                 temp = this._guiStage._elements[i];
                 if(temp.isActive() && keysState.ENTER){
                     temp.triggerCallback();
                 }
             }
+
+            for(i = 0; i < this._guiStage._elements.length; i+=1){
+            temp = this._guiStage._elements[i];
+            if(temp.isEnabled() && temp.isActive()){
+                if(this._displacement.scale.y < 6){
+                    this._displacement.scale.y += 0.1;
+                }
+                else{
+                    this._displacement.scale.y = 1;
+                }
+                temp._text.filters = [this._displacement];
+            }
+        }
         }
         
         return {action: this._onUpdateAction, params: this._nextScreenParams, changeTo: this._nextScreen, playSound: this._sounds};

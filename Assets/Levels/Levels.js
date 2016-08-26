b5624a790b67edea67313b07b95147bce885e633
Devanist <devan93@gmail.com>
2016-08-26 19:46:09 +0200

    added new level

--- Assets/Levels/Levels.js
+++ Assets/Levels/Levels.js
@@ -1,11 +1,13 @@
 define([
     "json!Assets/Levels/c1_l1.json",
+    "json!Assets/Levels/c1_l2.json",
     "json!Assets/Levels/test.json"
 ], 
-function(c1_l1, test){
+function(c1_l1, c1_l2, test){
 
     var Levels = {
         C1_L1: c1_l1,
+        C1_L2: c1_l2,
         test: test
     };
 

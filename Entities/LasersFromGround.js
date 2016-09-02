d1103c16e57c5ea84abb7f9d091cf92af915f050
Devanist <devan93@gmail.com>
2016-09-03 01:43:58 +0200

    fixed displaying of ground sprite in lasers from ground

--- Entities/LasersFromGround.js
+++ Entities/LasersFromGround.js
@@ -1,35 +1,39 @@
 define(['Entities/Obstacle'], function(Obstacle){
 
     /**
+     * Object that fires missiles. When missiles are in contact with player, he loses.
      * @class
      * @memberOf Entities
      * @extends Entities.Obstacle
+     * @param {Number} id Unique identifier of element
      */
     var LasersFromGround = function(id){
+        this._firstGround = new PIXI.Sprite.fromFrame("lazor_00000");
         this._lazorOne = new PIXI.Sprite.fromFrame("lazor");
         this._lazorOne.position.x += 24;
         this._lazorTwo = new PIXI.Sprite.fromFrame("lazor");
         this._lazorTwo.position.x += 45;
         this._lazorThree = new PIXI.Sprite.fromFrame("lazor");
         this._lazorThree.position.x += 80;
-        this._ground = new PIXI.Sprite.fromFrame("lazor_00000");
+        this._ground = new PIXI.Sprite.fromFrame("lazor_up");
 
-        this._lazorOne.position.y += 290;
-        this._lazorTwo.position.y += 327;
-        this._lazorThree.position.y += 300;
+        this._lazorOne.position.y += 26;
+        this._lazorTwo.position.y += 60;
+        this._lazorThree.position.y += 40;
 
         this._lazors = [this._lazorOne, this._lazorTwo, this._lazorThree];
 
         Obstacle.call(this, id, [
+            this._firstGround,
             this._lazorOne,
             this._lazorTwo,
             this._lazorThree,
             this._ground
         ]);
-        this._data.maxHeight = 320;
+        this._data.maxHeight = 380;
         this._data.type = "LasersFromGround";
         this._data.inheritedTypes.push(this._data.type);
-        this._data.animationSpeed = -5;
+        this._data.animationSpeed = -10;
         this._data.state.collisionItems = [
             {
                 initialPosition: {
@@ -95,13 +99,6 @@ define(['Entities/Obstacle'], function(Obstacle){
     _p.update = function(){
 
         for(let i = 0; i < this._data.state.collisionItems.length; i++){
-            // this._data.state.collisionItems[i].currentPosition = {
-            //         x: this._lazors[i].position.x,
-            //         y: this._lazors[i].position.y,
-            //         ex: this._lazors[i].position.x + this._lazors[i].width,
-            //         ey: this._lazors[i].position.y + this._lazors[i].height
-            // };
-
             this._lazors[i].position.x = this._data.state.collisionItems[i].currentPosition.x;
             this._lazors[i].position.y = this._data.state.collisionItems[i].currentPosition.y;
         }

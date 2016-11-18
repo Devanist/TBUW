7831bc0f212c8d69cf0cb86a2824ba457e2fbc91
Devanist <devan93@gmail.com>
2016-11-18 21:18:46 +0100

    functional update scene method

--- Screens/GUIEditor.js
+++ Screens/GUIEditor.js
@@ -158,10 +158,67 @@ function(Screen, Stage, GUI, Spritesheet, $){
     _p.updateStage = function(stage){
 
         if(stage === "background"){
-            console.log(this._backgroundElements);
+
+            this._background.removeAll();
+
+            this._backgroundElements.
+                map(configToElements).
+                forEach( (item) => {
+                    this._background.add(item);
+                });
         }
         else if(stage === "gui"){
-            console.log(this._guiElements);
+
+            this._guiStage.removeAll();
+
+            this._guiElements.
+                map(configToElements).
+                forEach( (item) => {
+                    this._guiStage.add(item);
+                });
+        }
+
+        function configToElements( obj ){
+
+            let temp;
+
+            let small = 1;
+            if(window.innerWidth <= 640){
+                small = 2;
+            }
+
+            if(obj.options && obj.options.fontSize && obj.options.fontFamily){
+                obj.options.font = `${parseInt(obj.options.fontSize) / small}px ${obj.options.fontFamily}`;
+            }
+
+            let texture = null;
+            if(obj.texture !== null && obj.texture !== undefined){
+                texture = PIXI.Texture.fromFrame(obj.texture);
+            }
+
+            switch(obj.type){
+                case "image":
+                    temp = new GUI.Image(obj.id, obj.position, texture);
+                    break;
+                case "label":
+                    temp = new GUI.Label(obj.id, obj.position, obj.text, obj.options);
+                    break;
+                case "button":
+                    temp = new GUI.Button(obj.id, obj.position, texture, obj.text, obj.options);
+                    break;
+                default: 
+                    console.error(`Bad type: ${obj.type}`);
+                    break;
+            }
+
+            if(obj.move){
+                temp.move(obj.move);
+            }
+            if(obj.visible !== undefined && obj.visible !== null){
+                temp.display(obj.visible);
+            }
+            return temp;
+
         }
     };
 
